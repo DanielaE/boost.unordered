ea5191393161b69ac13945311bfb59d8bd46a4ff
Daniela Engert <dani@ngrt.de>
2017-12-26 14:40:36 +0100

    silence deprecation warnings when compiled by msvc14.1 in C++17 mode
    Signed-off-by: Daniela Engert <dani@ngrt.de>
    (cherry picked from commit bb31713c33d034fe2c39e4cedf92dc09b1b89a61)

--- test/helpers/prefix.hpp
+++ test/helpers/prefix.hpp
@@ -9,3 +9,13 @@
 #pragma warning(disable : 4201) // nonstandard extension used :
                                 // nameless struct/union
 #endif
+
+// the tests trigger a lot of deprecation warnings when compiled with msvc in C++17 mode
+#if defined(_MSVC_LANG) && _MSVC_LANG > 201402
+// warning STL4007: Many result_type typedefs and all argument_type, first_argument_type, and second_argument_type typedefs are deprecated in C++17
+#define _SILENCE_CXX17_ADAPTOR_TYPEDEFS_DEPRECATION_WARNING
+#if BOOST_UNORDERED_USE_ALLOCATOR_TRAITS != 1
+// warning STL4010: Various members of std::allocator are deprecated in C++17
+#define _SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING
+#endif
+#endif
